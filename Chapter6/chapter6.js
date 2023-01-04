3ff89393a74170bbc24b1bd4201f199a882cc575
austenv6 <58868094+austenv6@users.noreply.github.com>
2023-01-03 21:52:17 -0600

    more ch6

--- Chapter6/chapter6.js
+++ Chapter6/chapter6.js
@@ -34,9 +34,9 @@ let protoRabbit = {
       console.log(`The ${this.type} rabbit says '${line}'`);
     }
   };
-let killerRabbit = Object.create(protoRabbit);
-killerRabbit.type = "killer";
-killerRabbit.speak("SKREEEE!");
+// let killerRabbit = Object.create(protoRabbit);
+// killerRabbit.type = "killer";
+// killerRabbit.speak("SKREEEE!");
 
 function makeRabbit(type) {
     let rabbit = Object.create(protoRabbit);
@@ -44,11 +44,133 @@ function makeRabbit(type) {
     return rabbit;
 }
 
-function Rabbit(type) {
-    this.type = type;
+// function Rabbit(type) {
+//     this.type = type;
+// }
+// Rabbit.prototype.speak = function(line) {
+//     console.log(`The ${this.type} rabbit says '${line}'`);
+// };
+
+// let weirdRabbit = new Rabbit("weird");
+
+class Rabbit {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`);
+    }
 }
-Rabbit.prototype.speak = function(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`);
+
+let killerRabbit = new Rabbit("killer");
+let blackRabbit = new Rabbit("black");
+
+let object = new class { getWord() { return "hello"; } };
+console.log(object.getWord());
+
+Rabbit.prototype.teeth = "small";
+console.log(killerRabbit.teeth);
+
+killerRabbit.teeth = "long, sharp and bloody";
+console.log(killerRabbit.teeth);
+
+console.log(blackRabbit.teeth);
+console.log(Rabbit.prototype.teeth);
+
+console.log(killerRabbit.toString());
+
+// maps (keys)
+let ages = new Map();
+ages.set("Boris", 39);
+ages.set("Liang", 22);
+ages.set("Julia", 62);
+console.log(`Julia is ${ages["Julia"]}`);
+console.log("Is Jack's age known?", "Jack" in ages);
+console.log("Is toString's age known?", "toString" in ages);
+console.log(ages.toString());
+
+//polymorphism
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+};
+console.log(String(blackRabbit));
+
+//Symbols
+let sym = Symbol("name");
+console.log(sym == Symbol("name"));
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]);
+
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+    return `${this.length} cm of blue yarn`;
 };
 
-let weirdRabbit = new Rabbit("weird");
+console.log([1,2].toString());
+console.log([1,2][toStringSymbol]());
+
+let stringObject = {
+    [toStringSymbol]() { return "a jute rope"; }
+};
+console.log(stringObject[toStringSymbol]());
+
+//iterator interface
+let okIterator = "OK"[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
+
+//iterable data structure
+class Matrix {
+    constructor(width, height, element = (x,y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x,y);
+            }
+        }
+    }
+
+    get(x,y) {
+        return this.content[y * this.width + x];
+    }
+    set(x,y,value) {
+        return this.content[y * this.width + x] = value;
+    }
+}
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return {done: true};
+
+        let value = {x: this.x,
+                     y: this.y,
+                     value: this.matrix.get(this.x, this.y)};
+        this.x++;
+        if (this.x == this.matrix.width) {
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done: false};
+    }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+    return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x,y) => `value ${x},${y}`);
+for (let {x, y, value} of matrix) {
+    console.log(x, y, value);
+}
+
+//getters, setters and statics
